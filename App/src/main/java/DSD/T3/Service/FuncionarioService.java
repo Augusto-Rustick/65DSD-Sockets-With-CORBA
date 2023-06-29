50f7d29d449fe9d28dffb5e8b89d08f7d4970a75
Augusto-Rustick <a.rustick@outlook.com>
2023-06-29 01:40:07 -0300

    funcionarioService

--- App/src/main/java/DSD/T3/Service/FuncionarioService.java
+++ App/src/main/java/DSD/T3/Service/FuncionarioService.java
@@ -1,4 +1,85 @@
 package DSD.T3.Service;
 
-public class FuncionarioService {
+
+import DSD.T3.Entity.FuncionarioPOA;
+
+public class FuncionarioService extends FuncionarioPOA {
+
+   private String ctps;
+   private int quantidadeVendas;
+   private int id;
+   private String cpf;
+   private String nome;
+   private String endereco;
+   private int departamento;
+
+   @Override
+   public String ctps() {
+      return ctps;
+   }
+
+   @Override
+   public void ctps(String ctps) {
+      this.ctps = ctps;
+   }
+
+   @Override
+   public int quantidadeVendas() {
+      return quantidadeVendas;
+   }
+
+   @Override
+   public void quantidadeVendas(int quantidadeVendas) {
+      this.quantidadeVendas = quantidadeVendas;
+   }
+
+   @Override
+   public int id() {
+      return id;
+   }
+
+   @Override
+   public void id(int id) {
+      this.id = id;
+   }
+
+   @Override
+   public String cpf() {
+      return cpf;
+   }
+
+   @Override
+   public void cpf(String cpf) {
+      this.cpf = cpf;
+   }
+
+   @Override
+   public String nome() {
+      return nome;
+   }
+
+   @Override
+   public void nome(String nome) {
+      this.nome = nome;
+   }
+
+   @Override
+   public String endereco() {
+      return endereco;
+   }
+
+   @Override
+   public void endereco(String endereco) {
+      this.endereco = endereco;
+   }
+
+   @Override
+   public int departamento() {
+      return departamento;
+   }
+
+   @Override
+   public void departamento(int departamento) {
+      this.departamento = departamento;
+   }
 }
